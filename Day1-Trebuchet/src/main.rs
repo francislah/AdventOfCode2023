7bdf590b26686be45042f9bd6990300b147199cd
Francis Lahoud <flahoud@student.42quebec.com>
2023-12-01 14:29:21 -0500

    part 2 solved

--- Day1-Trebuchet/src/main.rs
+++ Day1-Trebuchet/src/main.rs
@@ -18,15 +18,36 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+fn search_numbers(line : &str) -> i32 {
+    let numbers = vec!["one", "two", "three", "four", "five", "six",
+                       "seven", "eight", "nine"];
+    for (index, number) in numbers.iter().enumerate() {
+        if line.starts_with(number) {
+            return index as i32 + 1
+        }
+    }
+    0
+}
 fn get_calibration(line: &str) -> i32 {
     let mut calibration = 0;
-    for c in line.chars() {
+    let len = line.len();
+    for (i, c) in line.chars().enumerate() {
+        let tmp = search_numbers(&line[i..]);
+        if tmp > 0 {
+            calibration = tmp * 10;
+            break;
+        }
         if c.is_numeric() {
             calibration += (c as i32 - 48) * 10;
             break;
         }
     }
-    for c in line.chars().rev() {
+    for (i, c) in line.chars().rev().enumerate() {
+        let tmp =  search_numbers(&line[(len - i)..]);
+        if tmp > 0 {
+            calibration += tmp;
+            break;
+        }
         if c.is_numeric() {
             calibration += c as i32 - 48;
             break;
