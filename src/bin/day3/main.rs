f8051d733e3ae9a336985c4d36e64e144749e0c5
Francis Lahoud <flahoud@student.42quebec.com>
2023-12-03 16:47:40 -0500

    part 2 solved - bad coding

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,10 +1,13 @@
 extern crate aoc2023;
 
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::io::{prelude::*, BufReader};
 use aoc2023::my_lib;
 
 #[derive(Default, Debug)]
 struct Strings {
+    row: usize,
     prev: String,
     curr: String,
     next: String
@@ -12,6 +15,7 @@ struct Strings {
 fn main() {
     let file = my_lib::open_file("src/bin/day3/input.txt");
     let reader = BufReader::new(file);
+    let mut gears: HashMap<(usize, usize), (usize, i32)> = HashMap::new();
     let schema: Vec<String> = reader.lines().map(|line| {
         match line {
             Ok(line) => line,
@@ -20,30 +24,30 @@ fn main() {
     }).collect();
     let mut schema_lines = Strings{ ..Default::default() };
     let mut total = 0;
-    for line in schema.iter() {
+    for (i, line) in schema.iter().enumerate() {
         schema_lines.prev = schema_lines.curr;
         schema_lines.curr = schema_lines.next;
         schema_lines.next = line.clone();
         if schema_lines.curr.len() > 0 {
-            total += verify(&schema_lines);
+            schema_lines.row = i - 1;
+            total += verify(&schema_lines, &mut gears);
         }
-        // println!("index {}: {}", i, line);
     }
+    schema_lines.row += 1;
     schema_lines.prev = schema_lines.curr;
     schema_lines.curr = schema_lines.next;
     schema_lines.next = "".to_string();
-    total += verify(&schema_lines);
+    total += verify(&schema_lines, &mut gears);
     println!("Total is {}", total);
+    println!("Total value for gears is {}", gears.values().filter(|v| v.0 == 2).map(|v| v.1).sum::<i32>());
 }
 
-fn verify(schema_lines: &Strings) -> i32 {
+fn verify(schema_lines: &Strings, gears: &mut HashMap<(usize, usize), (usize, i32)>) -> i32 {
     let mut line_total = 0;
     let mut num = 0;
     let mut to_verify: bool = false;
     let mut pos = (0usize, 0usize);
     for (i, c) in schema_lines.curr.chars().enumerate() {
-
-        // if i > 0 && i == schema_lines.curr.len() - 1 { to_verify = true }
         if c.is_numeric() {
             if num == 0 { pos.0 = i }
             num = num * 10 + c as i32 - 48;
@@ -55,24 +59,12 @@ fn verify(schema_lines: &Strings) -> i32 {
         if to_verify || (i == schema_lines.curr.len() - 1 && num != 0) {
             let start = if pos.0 > 0 { pos.0 - 1 } else { 0 };
             let end = if pos.1 < schema_lines.curr.len() - 1 { pos.1 + 1 } else { pos.1 };
-            if verify_curr(&schema_lines, start, end) {
+            if verify_curr(&schema_lines, start, end, gears, num) {
                 line_total += num;
-                // println!("{}: true", num);
-            } else {
-                // println!("{}: false", num);
             }
             num = 0;
         }
         to_verify = false;
-            // println!("index: {} with char: {} and len {}", i, c, schema_lines.curr.len() )
-        // } else if num > 0 {
-        //     to_verify = true;
-        // }
-        // if to_verify == true {
-        //     num = 0;
-        //     to_verify = false
-        // }
-        // println!("{}", i);
     }
     line_total
 }
@@ -90,32 +82,37 @@ fn display_surroundings(schema: &Strings, start: usize, end: usize) {
     }
 }
 
-fn verify_curr(schema: &Strings, start: usize, end: usize) -> bool {
+fn verify_curr(schema: &Strings, start: usize, end: usize, gears: &mut HashMap<(usize, usize), (usize, i32)>, num : i32) -> bool {
     // display_surroundings(schema, start, end);
     let mut valid: bool;
     if !schema.curr[start..=end].starts_with(".") || !schema.curr[start..=end].ends_with(".") {
-        for c in schema.curr[start..=end].chars() {
-            if !c.is_numeric() && c != '.' {
-                return true
-            }
-        }
+        valid = find_symbol(&schema.curr[start..=end], gears, schema.row, start, num);
+        if valid { return true };
     }
     if schema.prev.len() > 0 {
-        valid = find_symbol(&schema.prev[start..=end]);
+        valid = find_symbol(&schema.prev[start..=end], gears, schema.row - 1, start, num);
         if valid { return valid };
     }
     if schema.next.len() > 0 {
-        valid = find_symbol(&schema.next[start..=end]);
+        valid = find_symbol(&schema.next[start..=end], gears, schema.row + 1, start, num);
         if valid { return valid };
     }
     false
 }
 
-fn find_symbol(line: &str) -> bool {
-    for c in line.chars() {
+fn find_symbol(line: &str, gears: &mut HashMap<(usize, usize), (usize, i32)>, row: usize, pos:usize, num: i32) -> bool {
+    for (i, c) in line.chars().enumerate() {
         if c.is_numeric() || c == '.' {
             continue
         } else {
+            if c == '*' {
+                match gears.get(&(row, (pos + i))) {
+                    Some(v) => {
+                        gears.insert((row, pos + i),(2, v.1 * num))
+                    },
+                    None => gears.insert((row, pos + i), (1, num))
+                };
+            }
             return true
         }
     }
