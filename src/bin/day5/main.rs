188d3f7e1fc8d3ee406f4cd1c937c25a5e508683
Francis Lahoud <flahoud@student.42quebec.com>
2023-12-06 06:50:54 -0500

    new direction

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -14,6 +14,12 @@ struct AlmanacItem {
     location: i32
 }
 
+struct ItemData {
+    start: Vec<i32>,
+    range: Vec<i32>,
+    dest: Vec<i32>
+}
+
 enum Item {
     SOIL,
     FERTILIZER,
@@ -30,40 +36,47 @@ fn main() {
     let mut seeds: Vec<i32> = vec![];
     let mut almanac_items: HashMap<i32, AlmanacItem> = HashMap::new();
     let mut current: Item = Item::SOIL;
-    lines.iter().for_each(|line| {
+    for line in lines {
         if line.starts_with("seeds: ") {
             add_seeds(&line[7..], &mut seeds);
             for seed in &seeds {
                 almanac_items.insert(*seed, AlmanacItem{..Default::default()});
             }
-        }
-        if line.ends_with(":") {
+        } else if line.ends_with(":") {
             if let Some(item) = line.trim_end_matches(':')
                 .split('-')
                 .nth(2)
                 .and_then(|word| word.split_once(' ')) {
-                    current = match item.0 {
-                        "soil" => Item::SOIL,
-                        "fertilizer" => Item::FERTILIZER,
-                        "water" => Item::WATER,
-                        "light" => Item::LIGHT,
-                        "temperature" => Item::TEMPERATURE,
-                        "humidity" => Item::HUMIDITY,
-                        "location" => Item::LOCATION,
-                        _ => panic!("Error: wrong item ({})", item.0)
-                    };
-                    println!("{}", item.0);
-            }
-        } else {
+                    current = set_current(&item.0)
+                };
+
+        } else if line.trim() == "" { continue; }
+        else {
             let mut vec: Vec<i32> = vec![];
             line.split_whitespace().for_each(|n| match n.parse::<i32>() {
-                Ok(n) => vec.push(n),
-                Err(e) => panic!("{}", e)
+                Ok(n) => {println!("{}", n); vec.push(n)},
+                Err(e) => panic!("{}: {}", e, n)
             });
-
+            println!("{}", match vec.iter().next() {Some(n) => n, None => &0});
+            // if vec.len() == 0 {println!("vec 0 = {}", line)}
+            // else println!()
+            println!("to look s: {}", vec[1]);
         }
-    });
+    };
 
+}
+
+fn set_current (item: &str) -> Item {
+    match item {
+        "soil" => Item::SOIL,
+        "fertilizer" => Item::FERTILIZER,
+        "water" => Item::WATER,
+        "light" => Item::LIGHT,
+        "temperature" => Item::TEMPERATURE,
+        "humidity" => Item::HUMIDITY,
+        "location" => Item::LOCATION,
+        _ => panic!("Error: wrong item ({})", item)
+    }
 }
 
 fn add_seeds(numbers: &str, seeds: &mut Vec<i32>) {
